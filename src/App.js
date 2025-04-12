a9093f9bfc3245906e44c3316cf7e2895e342c68
QunnTr <nauq261@gmail.com>
2025-04-12 11:25:18 +0700

    add asset and nav

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Navbar } from "./Components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a>Hello world</a>
-      </header>
+    <div>
+      <Navbar />
     </div>
   );
 }
